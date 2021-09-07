a2f71904cae1f2fe5407486796e32610969b9a6d
AMYSoft <info@amy98.ir>
2021-09-07 14:38:54 +0430

    Update code.

--- assets/php/config.php
+++ assets/php/config.php
@@ -3,6 +3,7 @@ $dbh = "localhost";
 $dbu = "root";
 $dbp = "";
 $dbn = "ara";
+$table_prefix = "ara";
 $pdo = new PDO("mysql:host = $dbh;dbname=$dbn;" , $dbu , $dbp);
 $server = "http://localhost/";
 ?>
