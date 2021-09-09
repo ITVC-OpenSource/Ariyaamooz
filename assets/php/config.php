af8215514f0005d73a7a49813db4f3f8e18a2ddc
AMYSoft <info@amy98.ir>
2021-09-09 19:56:26 +0430

    Update code.

--- assets/php/config.php
+++ assets/php/config.php
@@ -5,5 +5,5 @@ $dbp = "";
 $dbn = "ara";
 $table_prefix = "ara";
 $pdo = new PDO("mysql:host = $dbh;dbname=$dbn;" , $dbu , $dbp);
-$server = "http://localhost/";
+$server = "http://localhost/aa/";
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-include('assets/php/config.php');
+include("assets/php/router.php");
+ROUTE::get("/" , function() {
+  include(__DIR__ . "/assets/pages/index.php");
+});
+ROUTE::get("/a" , function() {
+  include(__DIR__ . "/assets/pages/a.php");
+});
 ?>
