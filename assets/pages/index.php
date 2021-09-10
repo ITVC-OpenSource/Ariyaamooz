6f2b5bbf6f3e2d94c7d63633f953251f32e25118
AMYSoft <info@amy98.ir>
2021-09-10 21:32:15 +0430

    Update code.

--- assets/pages/index.php
+++ assets/pages/index.php
@@ -1,3 +1,11 @@
-<?php
-echo "string";
-?>
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+  <head>
+    <meta charset="utf-8">
+    <title>آریاآموز</title>
+    <link rel="icon" href="assets/img/favicon.ico">
+  </head>
+  <body>
+
+  </body>
+</html>
