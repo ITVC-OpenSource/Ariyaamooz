1066a8a5380b8da3fff7614d27c5d93e84f2b471
AMYSoft <info@amy98.ir>
2021-09-15 08:34:41 +0430

    Update code.

--- assets/php/config.php
+++ assets/php/config.php
@@ -11,4 +11,18 @@ $dbc = new PDO("mysql:host = $dbh;dbname=$dbn;" , $dbu , $dbp);
 $connection = $dbc;
 $server = "http://localhost/aa/";
 $htaccess_code_for_disable_php_flags = "php_flag display_errors off";
+if (!isset($_SESSION["uname"])) {
+    header("location: ../");
+}else {
+    $uname = $_SESSION["uname"];
+    $user_res = $dbc->query("SELECT * FROM `users` WHERE uname = '$uname'");
+    $user = $user_res->fetch(PDO::FETCH_ASSOC);
+    if ($user['type'] = "admin") {
+    include(__DIR__ . '/../pages/admin.php');
+    }elseif ($user['type'] = "student") {
+    include(__DIR__ . '/../pages/student.php');
+    }elseif ($user['type'] = "teacher") {
+    include(__DIR__ . '/../pages/teahcer.php');
+    }
+}
 ?>
