6cfd6b2652fd3d1dee4382994254f7dc4a6c966b
AMYSoft <info@amy98.ir>
2021-09-15 09:51:05 +0430

    Update code.

--- assets/pages/panel.php
+++ assets/pages/panel.php
@@ -1,3 +1,10 @@
 <?php
 include(__DIR__ . "/../php/config.php");
+if ($user['type'] = "admin") {
+    include(__DIR__ . '/../pages/admin.php');
+} elseif ($user['type'] = "student") {
+    include(__DIR__ . '/../pages/student.php');
+} elseif ($user['type'] = "teacher") {
+    include(__DIR__ . '/../pages/teahcer.php');
+}
 ?>
--- assets/php/config.php
+++ assets/php/config.php
@@ -17,12 +17,5 @@ if (!isset($_SESSION["uname"])) {
     $uname = $_SESSION["uname"];
     $user_res = $dbc->query("SELECT * FROM `users` WHERE uname = '$uname'");
     $user = $user_res->fetch(PDO::FETCH_ASSOC);
-    if ($user['type'] = "admin") {
-        include(__DIR__ . '/../pages/admin.php');
-    } elseif ($user['type'] = "student") {
-        include(__DIR__ . '/../pages/student.php');
-    } elseif ($user['type'] = "teacher") {
-        include(__DIR__ . '/../pages/teahcer.php');
-    }
 }
 ?>
